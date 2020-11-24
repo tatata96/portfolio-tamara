984990f7e041da2852138beaac1de228575e0009
tatata96 <53620159+tatata96@users.noreply.github.com>
2020-11-24 16:32:15 +0300

    Update script.js
    fixed errors

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById('toggle-icon');
-const image1 = document.getElementById('image1');
-const image2 = document.getElementById('image2');
-const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
 function toggleDarkLightMode(isDark) {
@@ -11,16 +8,9 @@ function toggleDarkLightMode(isDark) {
     textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode';
     isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
-    isDark ? imageMode('dark') : imageMode('light');
 
 }
 
-function imageMode(color) {
-
-    image1.src = 'img/undraw_proud_coder_' + color + '.svg';
-    image2.src = 'img/undraw_proud_coder_' + color + '.svg';
-    image3.src = 'img/undraw_proud_coder_' + color + '.svg';
-}
 
 
 function switchTheme(event) {
